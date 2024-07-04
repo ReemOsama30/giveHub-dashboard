03ad6fda4ca832c73d3b14063ace311b4d679d98
Reem <reem.osama3052001@gmail.com>
2024-07-04 21:00:21 +0300

    register and login design done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { UsersComponent } from '../components/users/users.component';
+import { HomeComponent } from '../components/home/home.component';
+import { RegisterComponent } from '../components/register/register.component';
 
 export const routes: Routes = [
-
+{path:"",redirectTo:"home", pathMatch: 'full'},
 {path:"login",component:LoginComponent},
 
 {
 path:"users",component:UsersComponent
+}
+
+,{
+    path:"home",component:HomeComponent
+},{
+    path:"register",component:RegisterComponent
 }
 
 
