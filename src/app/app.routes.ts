87012e8df410a3e293188e97ba2b2a4d6919179d
Reem <reem.osama3052001@gmail.com>
2024-07-04 17:42:15 +0300

    finish users table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
+import { UsersComponent } from '../components/users/users.component';
 
 export const routes: Routes = [
 
-{path:"login",component:LoginComponent}
+{path:"login",component:LoginComponent},
+
+{
+path:"users",component:UsersComponent
+}
 
 
 
---
+++ src/components/users/users.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../Services/UserService/users.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent implements OnInit {
+
+   users:any=[];
+  constructor(private userService:UsersService)
+  {
+
+  }
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+  getAllUsers(): void {
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users.message; 
+        console.log(this.users); 
+      },
+      error: (error) => { 
+        console.error('Error retrieving users:', error);
+      
+      }
+    });
+  }
+
+
+  getFullImageUrl(relativePath: string): string {
+    return `https://localhost:44377${relativePath}`;
+  }
+
+  getBadgeClass(accountType: string): string {
+    return accountType === 'donor' ? 'loyal' : 'team';
+  }
+
+}
