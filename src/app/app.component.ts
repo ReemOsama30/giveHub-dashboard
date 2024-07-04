4175c5454f0f2a4f15434224bdef2ce1f3007816
Reem <reem.osama3052001@gmail.com>
2024-07-04 15:17:32 +0300

    fix icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { HomeComponent } from '../components/home/home.component';
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,HomeComponent,LoginComponent,RegisterComponent],
+  imports: [RouterOutlet,NavbarComponent,HomeComponent,LoginComponent,RegisterComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
