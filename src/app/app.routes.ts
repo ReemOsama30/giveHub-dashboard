ee6956556f415c6d1ce030ccefd2d5dbee06b657
SeraviaSamuel <seravia.sameh18@gmail.com>
2024-07-05 22:46:49 +0300

    projects details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,13 @@ import { LoginComponent } from '../components/login/login.component';
 import { UsersComponent } from '../components/users/users.component';
 import { HomeComponent } from '../components/home/home.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
 
 export const routes: Routes = [
 {path:"",redirectTo:"home", pathMatch: 'full'},
 {path:"login",component:LoginComponent},
-
-{
-path:"users",component:UsersComponent
-}
-
-,{
-    path:"home",component:HomeComponent
-},{
-    path:"register",component:RegisterComponent
-}
-
-
-
-
+{path:"users",component:UsersComponent},
+{path:"home",component:HomeComponent},
+{path:"register",component:RegisterComponent},
+{path:"projects",component:ProjectsComponent}
 ];
